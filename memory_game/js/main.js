4051df550501c3233201db5c6ab06e58e4e14dc4
Waldron <ube398@dca90475f0c5.cof.ds.capitalone.com>
2017-07-19 15:47:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+
+
+var cardOne;
+var cardTwo;
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+cardOne = cards[0];
+cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }
+  else{
+    alert("Sorry try again!");
+  }
+}
