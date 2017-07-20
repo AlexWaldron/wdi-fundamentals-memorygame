adbcd6e5e19beb35771492ca7326b976a7973f60
Waldron <ube398@dca90475f0c5.cof.ds.capitalone.com>
2017-07-20 10:54:56 -0400

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,28 @@
 
 
-var cardOne;
-var cardTwo;
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-cardOne = cards[0];
-cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2){
-  if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-  }
-  else{
-    alert("Sorry try again!");
-  }
+var checkForMatch = function(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
+}
+
+var cardId;
+
+var flipCard = function(cardId){
+
+cardsInPlay.push(cards[cardId]);
+
+console.log("user flipped " + cardsInPlay[cardsInPlay.length - 1]);
+
+
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
